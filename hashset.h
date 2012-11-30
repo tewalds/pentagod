ef082b44046e6366e33ca05e462c5dd967a57731
Timo Ewalds <timo@spotify.com>
2012-11-30 01:51:33 +0100

    Mix the bits into the hashset for a better distribution, fewer collisions

--- hashset.h
+++ hashset.h
@@ -4,12 +4,13 @@
 #include <stdint.h>
 
 class HashSet {
-	unsigned int size;
-	unsigned int mask;
-	uint64_t * table;
+	unsigned int size;  // how many slots there are, must be a power of 2
+	unsigned int mask;  // a mask for the size, ie size-1
+	unsigned int count; // how many of the slots are filled in
+	uint64_t * table;   // pointer to the actual memory of slots
 
 public:
-	HashSet() : size(0), mask(0), table(NULL) { }
+	HashSet() : size(0), mask(0), count(0), table(NULL) { }
 	HashSet(unsigned int s){ init(s); }
 	~HashSet(){
 		if(table)
@@ -20,6 +21,7 @@ public:
 	void init(unsigned int s){
 		size = roundup(s)*4;
 		mask = size-1;
+		count = 0;
 		table = new uint64_t[size];
 		for(unsigned int i = 0; i < size; i++)
 			table[i] = 0;
@@ -27,11 +29,13 @@ public:
 
 	//returns whether it was set successfully, fails if it is already in the set
 	bool add(uint64_t h){
+		h = mix_bits(h);
 		unsigned int i = h & mask;
 		while(true){
 			uint64_t t = table[i];
 			if (t == 0) {
 				table[i] = h;
+				count++;
 				return true;
 			} else if (t == h) {
 				return false;
@@ -42,17 +46,28 @@ public:
 
 	//slightly faster than add, but may double-add an entry
 	void set(uint64_t h){
+		h = mix_bits(h);
 		unsigned int i = h & mask;
 		while(table[i] != 0)
 			i = (i+1) & mask;
 		table[i] = h;
+		count++;
 	}
 
 	bool exists(uint64_t h){
+		h = mix_bits(h);
 		for(unsigned int i = h & mask; table[i]; i = (i+1) & mask)
 			if(table[i] == h)
 				return true;
 		return false;
+	}
+
+	//give a more even distribution over the bitspace
+	static uint64_t mix_bits(uint64_t h){
+		h ^= (h >> 17);
+		h ^= (h << 31);
+		h ^= (h >>  8);
+		return h;
 	}
 
 	//round a number up to the nearest power of 2

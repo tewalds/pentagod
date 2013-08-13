428830e6e01489398b3064df7eefc619dc34bc17
Timo Ewalds <timo@ewalds.ca>
2013-08-12 22:20:03 -0400

    Let params be set for the three agents

--- gtpagent.cpp
+++ gtpagent.cpp
@@ -153,7 +153,40 @@ GTPResponse PentagoGTP::gtp_pv(vecstr args){
 	return GTPResponse(true, pvstr);
 }
 
-GTPResponse PentagoGTP::gtp_player_params(vecstr args){
+GTPResponse PentagoGTP::gtp_params(vecstr args){
+	if(dynamic_cast<AgentAB   *>(agent)) return gtp_ab_params(args);
+	if(dynamic_cast<AgentMCTS *>(agent)) return gtp_mcts_params(args);
+	if(dynamic_cast<AgentPNS  *>(agent)) return gtp_pns_params(args);
+
+	return GTPResponse(false, "Unknown Agent type");
+}
+
+GTPResponse PentagoGTP::gtp_ab_params(vecstr args){
+	AgentAB * ab =  dynamic_cast<AgentAB *>(agent);
+
+	if(args.size() == 0)
+		return GTPResponse(true, string("\n") +
+			"Set player parameters, eg: params -r 4\n" +
+			"  -M --maxmem      Max memory in Mb to use for the tree              [" + to_str(ab->memlimit/(1024*1024)) + "]\n" +
+			"  -r --randomness  How many bits of randomness to add to the eval    [" + to_str(ab->randomness) + "]\n"
+			);
+
+	string errs;
+	for(unsigned int i = 0; i < args.size(); i++) {
+		string arg = args[i];
+
+		if((arg == "-M" || arg == "--maxmem") && i+1 < args.size()){
+			ab->set_memlimit(from_str<uint64_t>(args[++i])*1024*1024);
+		}else if((arg == "-r" || arg == "--randomness") && i+1 < args.size()){
+			ab->randomness = from_str<int>(args[++i]);
+		}else{
+			return GTPResponse(false, "Missing or unknown parameter");
+		}
+	}
+	return GTPResponse(true, errs);
+}
+
+GTPResponse PentagoGTP::gtp_mcts_params(vecstr args){
 	AgentMCTS * mcts =  dynamic_cast<AgentMCTS *>(agent);
 
 	if(args.size() == 0)
@@ -227,3 +260,44 @@ GTPResponse PentagoGTP::gtp_player_params(vecstr args){
 	return GTPResponse(true, errs);
 }
 
+GTPResponse PentagoGTP::gtp_pns_params(vecstr args){
+	AgentPNS * pns =  dynamic_cast<AgentPNS *>(agent);
+
+	if(args.size() == 0)
+		return GTPResponse(true, string("\n") +
+			"Update the pns solver settings, eg: pns_params -m 100 -s 0 -d 1 -e 0.25 -a 2 -l 0\n"
+			"  -m --memory   Memory limit in Mb                                       [" + to_str(pns->memlimit/(1024*1024)) + "]\n"
+			"  -t --threads  How many threads to run                                  [" + to_str(pns->numthreads) + "]\n"
+			"  -s --ties     Which side to assign ties to, 0 = handle, 1 = p1, 2 = p2 [" + to_str(pns->ties) + "]\n"
+			"  -d --df       Use depth-first thresholds                               [" + to_str(pns->df) + "]\n"
+			"  -e --epsilon  How big should the threshold be                          [" + to_str(pns->epsilon) + "]\n"
+//			"  -a --abdepth  Run an alpha-beta search of this size at each leaf       [" + to_str(pns->ab) + "]\n"
+			);
+
+	string errs;
+	for(unsigned int i = 0; i < args.size(); i++) {
+		string arg = args[i];
+
+		if((arg == "-t" || arg == "--threads") && i+1 < args.size()){
+			pns->numthreads = from_str<int>(args[++i]);
+			pns->reset_threads();
+		}else if((arg == "-m" || arg == "--memory") && i+1 < args.size()){
+			uint64_t mem = from_str<uint64_t>(args[++i]);
+			if(mem < 1) return GTPResponse(false, "Memory can't be less than 1mb");
+			pns->set_memlimit(mem*1024*1024);
+		}else if((arg == "-s" || arg == "--ties") && i+1 < args.size()){
+			pns->ties = from_str<int>(args[++i]);
+			pns->clear_mem();
+		}else if((arg == "-d" || arg == "--df") && i+1 < args.size()){
+			pns->df = from_str<bool>(args[++i]);
+		}else if((arg == "-e" || arg == "--epsilon") && i+1 < args.size()){
+			pns->epsilon = from_str<float>(args[++i]);
+//		}else if((arg == "-a" || arg == "--abdepth") && i+1 < args.size()){
+//			pns->ab = from_str<int>(args[++i]);
+		}else{
+			return GTPResponse(false, "Missing or unknown parameter");
+		}
+	}
+
+	return GTPResponse(true, errs);
+}
